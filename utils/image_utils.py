eed0844d00bbb61925cc4f5a08a95360102a20c6
cheverebe <alejandrorebecchi@gmail.com>
2016-12-17 14:15:52 -0300

    Object sprite limit position

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -7,10 +7,10 @@ class ImageUtils(object):
     def insert_sprite(sprite_img, position, background_img):
         dimensions = sprite_img.shape[:2]
         bg_dimensions = background_img.shape[:2]
-        x_0 = max(int(position[0] - float(dimensions[0]) / 2), 0)
-        y_0 = max(int(position[1] - float(dimensions[1]) / 2), 0)
-        x_1 = min(x_0 + dimensions[0], bg_dimensions[0]-1)
-        y_1 = min(y_0 + dimensions[1], bg_dimensions[1]-1)
+        x_0 = min(max(int(position[0] - float(dimensions[0]) / 2), 0), bg_dimensions[0]-dimensions[0])
+        y_0 = min(max(int(position[1] - float(dimensions[1]) / 2), 0), bg_dimensions[1]-dimensions[1])
+        x_1 = min(x_0 + dimensions[0], bg_dimensions[0])
+        y_1 = min(y_0 + dimensions[1], bg_dimensions[1])
         background_img[x_0:x_1, y_0:y_1] = sprite_img
 
     @staticmethod
